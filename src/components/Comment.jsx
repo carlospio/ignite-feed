093bf6a3284f49bf5da4390230b82151d16080ef
Carlos Pio <carlospiobenicio@gmail.com>
2023-09-08 12:25:53 -0300

    Os motores do React - Entendendo closures no React

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -12,7 +12,9 @@ export function Comment({content, onDeleteComment}){
         onDeleteComment(content)
     }
     function handleLikeComment(){
-        setLikeCount(likeCount + 1)
+       setLikeCount((state) => {
+        return state + 1
+       })
     }
 
     return (
